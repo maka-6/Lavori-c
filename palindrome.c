685e7b68c41112b4162b91fb74dcb60ac896006f
Youness <119128296+maka-6@users.noreply.github.com>
2024-02-12 00:21:50 +0100

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,68 +1,30 @@
 /*
 	Nome: palindrome
-	Copyright:
-	Autore:
-	Luogo:
-	Versione:
-	Componenti:
-	Data:
-	Descrizione:
+	Copyright: xx
+	Autore: Makaoui Youness
+	Luogo: xx
+	Versione: xx
+	Componenti: xx
+	Data: xx
+	Descrizione: Soluzione problema n. 9. Palindrome Number su leetcode
 */
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-/*
 bool isPalindrome( int x ){
 
-    int len = 0;
-    int xs = x;
+  if( x < 0 ){
+    return false;
+  }
 
-    while( xs != 0 ){
-      xs/=10;
-      len++;
-    }
+  long reversed = 0;
+  int xs = x;
 
-    char str[len];
-    snprintf( str, sizeof(str), "%d", x );
+  while (xs != 0) {
+    reversed = reversed * 10 + xs % 10;
+    xs /= 10;
+  }
 
-    if ( str[0] == str[len-1] ){
-    	
-    	printf("\nS");
-
-      	return true;
-      
-    } else {
-    	
-    	printf("\nN");
-		return false;
-    }
-}
-*/
-
-bool isPalindrome( int x ){
-
-    int len = 0;
-    int xs = x;
-
-    long long reversed = 0;
-
-    while (xs != 0) {
-        reversed *= 10 + xs % 10;
-        xs /= 10;
-    }
-    printf("%d",x);
-}
-
-
-int main(){
-	
-	int x;
-	
-	scanf("%d",&x);
-	
-	isPalindrome( x );
-	
-	return 0;
+  return (x == reversed);
 }
